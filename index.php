a3806798e5e1aa62a4effb4dd53a804314fe2bad
Victor Kulibaba <victor.kulibaba@gmail.com>
2017-11-26 17:54:16 +0300

    Выполнил задание #12

--- index.php
+++ index.php
@@ -1,93 +1,155 @@
 <?php
 
-    // подключение конфига
-    require_once 'config.php';
+    // ОПРЕДЕЛЕНИЕ ФУНКЦИЙ И ПАРАМЕТРОВ СТРАНИЦЫ ПО УМОЛЧАНИЮ
     
     // подключение библиотеки функций
     require_once 'functions.php';
+        
+    // подключение конфига
+    require_once 'config.php';
     
-    // обработка добавления задачи
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // заполнение $layout_data, $content_data и $content_template по умолчанию (для неавторизованного пользователя)
+    $layout_data = [
+        'title' => TITLE,
+        'overlay' => '',
+        'user' => [],
+        'content' => '',
+        'form' => ''
+    ];
+    $content_data = [];
+    $content_template = 'templates/guest.php';
+    
+    // ЧТЕНИЕ ДАННЫХ ИЗ СЕССИИ
+    
+    // запуск сессии
+    session_start();
+    
+    // если пользователь определен
+    if (isset($_SESSION['user'])) {
+        $user = $_SESSION['user'];
+        // передаем данные в layout
+        $layout_data['user'] = $_SESSION['user'];
+        // передаем связанные данные в content и выбираем соответствующий template
+        $content_data['array_projects'] = $array_projects;
+        $content_data['selected_project_id'] = 0;
+        $content_data['array_tasks'] = $array_tasks;
+        $content_data['array_tasks_to_show'] = $array_tasks;
+        $content_data['show_complete_tasks'] = $show_complete_tasks;
+        $content_template = 'templates/index.php';
+    };
+    
+    // ОБРАБОТКА POST ЗАПРОСОВ
+    
+    // обработка входа под учетной записью
+    if  ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['password'])) {
         
-        $required = ['name', 'project_id'];
-        $rules = ['date' => 'validateDate'];
-        $errors = [];
+        $required = ['email', 'password'];
+        $rules = ['email' => 'validateEmail'];
+        $errors = validateForm($required, $rules);
         
-        foreach ($_POST as $key => $value) {
-            if (in_array($key, $required) && $value == '') {
-                $errors[$key] = 'Пожалуйста, заполните это поле';
-                continue;
-            };
-            if (array_key_exists($key, $rules) && $value <> '') {
-                $result = call_user_func($rules[$key], $value);
-                if (!$result) {
-                    $errors[$key] = 'Пожалуйста, укажите корректное значение';
+        // если нет ошибок валидации
+        if(!count($errors)) {
+            
+            // ищем пользователя
+            $user_found = false;
+            require_once('userdata.php');
+            foreach($users as $user) {
+                // если нашелся user
+                if ($_POST['email'] == $user['email']) {
+                    $user_found = true;
+                    // проверяем пароль
+                    if (password_verify($_POST['password'], $user['password'])) {
+                        $_SESSION['user'] = $user;
+                        header('Location: index.php');
+                    } else {
+                        $errors['password'] = 'Вы ввели неверный пароль';
+                    };
                 };
+            };
+            
+            // если не нашелся user
+            if (!$user_found) {
+                $errors['email'] = 'Пользователь с указанным email не найден';
             };
         };
         
+        // если есть ошибки — оставляем форму открытой
+        if (count($errors)) {
+            $_GET['login'] = true;
+        };
+        
+    };
+    
+    // обработка добавления задачи
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' &&  isset($_POST['project_id'])) {
+        
+        $required = ['name', 'project_id'];
+        $rules = ['date' => 'validateDate'];
+        $errors = validateForm($required, $rules);
+        
+        // если нет ошибок валидации
         if (!count($errors)) {
+            
+            // добавление задачи в массив
             array_unshift($array_tasks, [
                 'name' => $_POST['name'],
                 'project' => $array_projects[$_POST['project_id']],
                 'date' => $_POST['date'],
                 'done' => false
             ]);
+            // обновление данных для заполнения шаблона
+            $content_data['array_tasks'] = $array_tasks;
             // сохранение вложенного файла
             if (isset($_FILES['preview'])) {
                 $file_path = __DIR__ . '/' . $_FILES['preview']['name'];
                 move_uploaded_file($_FILES['preview']['tmp_name'], $file_path);
             };
+            
+        // если есть ошибки валидации
         } else {
+            
+            // форма остается открытой
             $_GET['add'] = true;
         };
     };
     
+    // ОБРАБОТКА GET ПАРАМЕТРОВ
     
-    // заполнение $main_content_data по-умолчанию
-    $main_content_data = [
-        'show_complete_tasks' => $show_complete_tasks,
-        'array_tasks' => $array_tasks
-    ];
+    // обработка нажатия кнопки "Войти"
+    if (isset($_GET['login'])) {
+        $layout_data['overlay'] = ' class="overlay"';
+        $layout_data['form'] = use_template('templates/form_login.php', [
+            'errors' => $errors
+        ]);
+    };
     
-    // заполнение $layout_data по-умолчанию
-    $layout_data = [
-        'title' => TITLE,
-        'array_projects' => $array_projects,
-        'array_tasks' => $array_tasks,
-        'selected_project_id' => 0,
-        'content' => '',
-        'body_overlay' => '',
-        'task_input_form' => ''
-    ];
+    // обработка нажатия кнопки "Добавить задачу"
+    if (isset($_GET['add'])) {
+        $layout_data['overlay'] = ' class="overlay"';
+        $layout_data['form'] = use_template('templates/form_task_input.php', [
+            'errors' => $errors,
+            'array_projects' => $array_projects
+        ]);
+    };
     
     // если выбран проект для показа (параметр project_id)
     if (isset($_GET['project_id'])) {
         $selected_project_id = $_GET['project_id'];
         $project = $array_projects[$selected_project_id];
         if (isset($project)) {
-            $main_content_data['array_tasks'] = array_filter($array_tasks, function($task) use($project) {
+            $content_data['array_tasks_to_show'] = array_filter($array_tasks, function($task) use($project) {
                 return ($project == 'Все' || $task['project'] == $project);
             });
-            $layout_data['selected_project_id'] = $selected_project_id;
+            $content_data['selected_project_id'] = $selected_project_id;
         } else {
             header('HTTP/1.1 404 Not Found');
             die();
         };
     };
     
-    // если инициировано добавление задачи (параметр add)
-    if (isset($_GET['add'])) {
-        $layout_data['body_overlay'] = ' class="overlay"';
-        $layout_data['task_input_form'] = use_template('templates/task_input.php', [
-            'errors' => $errors,
-            'array_projects' => $array_projects
-        ]);
-    };
-    
     // получение основного содержания
-    $main_content = use_template('templates/index.php', $main_content_data);
-    $layout_data['content'] = $main_content;
+    $index_content = use_template($content_template, $content_data);
+    $layout_data['content'] = $index_content;
     
     // получение полного текста страницы
     $layout_content = use_template('templates/layout.php', $layout_data);
---
+++ templates/form_login.php
@@ -0,0 +1,42 @@
+<?php
+$email = $_POST['email'] ?? '';
+
+$error_email = $errors['email'] ?? '';
+$error_password = $errors['password'] ?? '';
+?>
+
+<div class="modal">
+    <button class="modal__close" type="button" name="button">Закрыть</button>
+
+    <h2 class="modal__heading">Вход на сайт</h2>
+
+    <form class="form" action="index.php" method="post">
+        <div class="form__row">
+            <label class="form__label" for="email">E-mail <sup>*</sup></label>
+
+            <input class="form__input
+                <?php if ($error_email) { echo 'form__input--error'; };?>
+            " type="text" name="email" id="email" value="<?=$email?>" placeholder="Введите e-mail">
+
+            <?php if ($error_email) : ?>
+                <p class="form__message"><?=$error_email?></p>
+            <?php endif; ?>
+        </div>
+
+        <div class="form__row">
+            <label class="form__label" for="password">Пароль <sup>*</sup></label>
+
+            <input class="form__input
+                <?php if ($error_password) { echo 'form__input--error'; };?>
+            " type="password" name="password" id="password" value="" placeholder="Введите пароль">
+            
+            <?php if ($error_password) : ?>
+                <p class="form__message"><?=$error_password?></p>
+            <?php endif; ?>
+        </div>
+
+        <div class="form__row form__row--controls">
+            <input class="button" type="submit" name="" value="Войти">
+        </div>
+    </form>
+</div>
