4eea10c52d1f837f44f5714ab8210f2a68349f1e
Victor Kulibaba <victor.kulibaba@gmail.com>
2017-12-02 01:53:14 +0300

    Задание #14

---
+++ queries.sql
@@ -0,0 +1,81 @@
+USE doingsdone;
+
+# Заполнение пользователей        
+INSERT INTO users (email, name, password)
+VALUES
+    ('ignat.v@gmail.com', 'Игнат', '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka'),
+    ('kitty_93@li.ru', 'Леночка', '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa'),
+    ('warrior07@mail.ru', 'Руслан', '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW');
+
+# Заполнение проектов
+INSERT INTO projects (name, users_id)
+SELECT
+    p.name,
+    u.id
+FROM
+    (SELECT 'Все' AS name
+    UNION SELECT 'Входящие'
+    UNION SELECT 'Учеба'
+    UNION SELECT 'Работа'
+    UNION SELECT 'Домашние дела'
+    UNION SELECT 'Авто') p
+    LEFT JOIN users u ON TRUE;
+
+# Заполнение задач
+INSERT INTO tasks (created, completed, name, deadline, users_id, projects_id)
+SELECT
+    NOW(),
+    t.completed,
+    t.name,
+    t.deadline,
+    u.id,
+    t.projects_id
+FROM
+    (SELECT
+        NULL AS completed,
+        'Собеседование в IT компании' AS name,
+        STR_TO_DATE('01.06.2018', '%d.%m.%Y') AS deadline,
+        4 AS projects_id
+    UNION SELECT
+        NULL,
+        'Выполнить тестовое задание',
+        STR_TO_DATE('25.05.2018', '%d.%m.%Y'),
+        4
+    UNION SELECT
+        STR_TO_DATE('2018-04-20 14:20:00', '%Y-%m-%d %H:%i:%s'),
+        'Сделать задание первого раздела',
+        STR_TO_DATE('21.04.2018', '%d.%m.%Y'),
+        3
+    UNION SELECT
+        NULL,
+        'Встреча с другом',
+        STR_TO_DATE('22.04.2018', '%d.%m.%Y'),
+        2
+    UNION SELECT
+        NULL,
+        'Купить корм для кота',
+        NULL,
+        5
+    UNION SELECT
+        NULL,
+        'Заказать пиццу',
+        NULL,
+        5) t
+    LEFT JOIN users u ON TRUE;
+    
+# Получить список из всех проектов для одного пользователя
+SELECT * FROM projects
+WHERE users_id = 1;
+    
+# Получить список из всех задач для одного проекта
+SELECT * FROM tasks
+WHERE projects_id = 2;
+
+# Пометить задачу как выполненную
+UPDATE tasks SET completed = NOW() WHERE id = 1;
+
+# Получить все задачи для завтрашнего дня
+SELECT * FROM tasks WHERE deadline = CURDATE() + INTERVAL 1 DAY;
+
+# Обновить название задачи по её идентификатору
+UPDATE tasks SET name = 'name_provided' WHERE id = 1;
--- schema.sql
+++ schema.sql
@@ -1,10 +1,14 @@
 CREATE DATABASE doingsdone;
 USE doingsdone;
+
+# Проекты
 CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name CHAR(32),
-    users_id INT FOREIGN KEY REFERENCES users (id) ON DELETE CASCADE
+    users_id INT
 );
+
+# Задачи
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     created DATETIME,
@@ -12,16 +16,27 @@ CREATE TABLE tasks (
     name CHAR(128),
     file TEXT,
     deadline DATE,
-    users_id INT FOREIGN KEY REFERENCES users (id) ON DELETE CASCADE,
-    projects_id INT FOREIGN KEY REFERENCES projects (id) ON DELETE CASCADE
+    users_id INT,
+    projects_id INT
 );
+
+# Пользователи
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name CHAR(128),
     email CHAR(128),
-    password CHAR(32),
+    password CHAR(60),
     contacts TEXT
 );
+
+# Индексы
 CREATE UNIQUE INDEX users_email ON users (email);
 CREATE INDEX tasks_name ON tasks (name);
 CREATE INDEX tasks_deadline ON tasks (deadline);
+
+# Внешние ключи
+ALTER TABLE projects
+	ADD FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE;
+ALTER TABLE tasks
+	ADD FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE,
+	ADD FOREIGN KEY (projects_id) REFERENCES projects (id) ON DELETE CASCADE;
